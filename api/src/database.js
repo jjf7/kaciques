57dda2f19b9085bc6e5ca6c9bb255d49d430e5a0
jjf7 <jfsistemas.contacto@gmail.com>
2021-04-21 14:28:33 -0400

    28017

--- api/src/database.js
+++ api/src/database.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 console.log('paso por aqui')
 mongoose
-  .connect("mongodb://localhost/bd_store_app", {
+  .connect("mongodb://localhost:28017/bd_store_app", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((db) => console.log("Database is connected..."))
-  .catch((e) => { console.error('App starting error:', e.stack);
+  .catch((e) => { console.error('App starting error:', e);
   process.exit(1)});
