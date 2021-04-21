aa211c8b20f429b646ab379ae4c4f9a39a059856
jjf7 <jfsistemas.contacto@gmail.com>
2021-04-21 16:45:07 -0400

    corechetes

--- api/src/database.js
+++ api/src/database.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
-mongoose
-  .connect("mongodb+srv://root:<EnNZRwLPzRxKz91Q>@cluster0.say68.mongodb.net/bd_store_app?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://root:EnNZRwLPzRxKz91Q@cluster0.say68.mongodb.net/bd_store_app?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((db) => console.log("Database is connected..."))
-  .catch((e) => { console.error('App starting error:', e);
-  process.exit(1)});
+  .catch((e) => { console.error('App starting error:', e)});
