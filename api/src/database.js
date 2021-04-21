91820f6089258e86675a79034228aeaaa67b89ef
jjf7 <jfsistemas.contacto@gmail.com>
2021-04-21 13:51:39 -0400

    index

--- api/src/database.js
+++ api/src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 console.log('paso por aqui')
 mongoose
-  .connect("mongodb://localhost:27017/bd_store_app", {
+  .connect("mongodb://127.0.0.1/bd_store_app", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
