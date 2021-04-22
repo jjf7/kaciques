28f44b4d4fb6ea7ad98376990f6b75218cd293fa
jjf7 <jfsistemas.contacto@gmail.com>
2021-04-22 11:56:27 -0400

    Remove src

--- api/src/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose.connect("mongodb+srv://root:EnNZRwLPzRxKz91Q@cluster0.say68.mongodb.net/bd_store_app?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => console.log("Database is connected..."))
-  .catch((e) => console.log(e))
--- api/src/index.js
+++
@@ -1,5 +0,0 @@
-import app from './app'
-import './database'
-app.listen(app.get('PORT'), () => {
-    console.log('Server on port:: ',app.get('PORT'))
-})
