fe1e8588ba4f64184b170b8675c847654506e469
jjf7 <jfsistemas.contacto@gmail.com>
2021-04-22 12:34:35 -0400

    'dele

--- front/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <div className="footer">
-            <span>Elaborado por: <a target="_blank" href="https://tupaginaonline.net">https://tupaginaonline.net</a></span>
-        </div>
-    )
-}
--- front/pages/faq.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout";
-
-export default function Faq() {
-    return (
-        <Layout>
-            <h3 className="title-h1">Faq</h3>
-        </Layout>
-    )
-}
